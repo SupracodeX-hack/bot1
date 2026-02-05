8896713274aa557a51c57a8be798c7a3f497f893
penka <fueyong90@gmail.com>
2026-02-05 23:07:59 +0000

    Ajout du fichier

--- bot.py
+++ bot.py
@@ -15,7 +15,8 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
 	await update.message.reply_text("Salut!! je suis le bot de hacker et son equipe. \n" "Envoie-moi un message et je te repondrai"
 )
 
-async def handle_message(update: Update, context: ContexTypes.DEFAULT_TYPE): """Reagit a tout les messages envoye par l'utilisateur"""
+async def handle_message(update: Update, context: ContexTypes.DEFAULT_TYPE): 
+	"""Reagit a tout les messages envoye par l'utilisateur"""
 	user_message=update.message.text
 	username=update.message.from_user.first_name
 
@@ -27,13 +28,14 @@ async def handle_message(update: Update, context: ContexTypes.DEFAULT_TYPE): """
 	await update.message.reply_text(bot_reply)
 
 
-def main(): """Fonction principale qui demarre le bot"""
+def main():
+	"""Fonction principale qui demarre le bot"""
 	app: ApplicationBuilder().token(TOKEN).build()
 	app.add_handler(commandler("start", start))
 
 app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
-	print("Bot en ligne et connecte a telegram....")
-	app.run_polling()
+print("Bot en ligne et connecte a telegram....")
+app.run_polling()
 
 
 if __name__=="__main__":main()
