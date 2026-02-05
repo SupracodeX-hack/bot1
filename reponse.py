611d646ff9a47b77fbfa48a04f2abf5e69036d9b
minkoulou <github.com>
2026-02-05 20:33:19 +0100

    fonction reponses du bot

---
+++ reponse.py
@@ -0,0 +1,26 @@
+def get_reponse(message):
+    message=message.lower()
+    if message=="bonjour" or message=="salut":
+        return "bonjour comment puis-je vous aider ?"
+    elif message=="hello" or message=="hi":
+        return "hello ravi de te vooir ici"
+
+    elif message=="aide":
+      return "je peux repondre a des messages simples comme bonjour,aide,info."
+
+    elif message=="info":
+        return "je suis un bot telegramme crée par notre équipe"
+
+    else:
+        return "Désolée je n'ai pas compris ton message"
+
+        print("Tape un message (ecris 'exit' pour quitter)")
+  
+        while True:
+            message=input("Toi:")  
+            if message=="exit":
+                print("test termine")
+                break
+
+                reponse=get_reponse(message)
+                print("Bot:",reponse)
